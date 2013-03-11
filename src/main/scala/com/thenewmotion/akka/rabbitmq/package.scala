5e92a173bcbf52135bfc7b75aa2a20770be91a7d
Yaroslav Klymko <t3hnar@gmail.com>
2013-03-11 14:08:21 +0200

    Add `createChannel` function for creating channelActor synchronously

--- src/main/scala/com/thenewmotion/akka/rabbitmq/package.scala
+++ src/main/scala/com/thenewmotion/akka/rabbitmq/package.scala
@@ -1,6 +1,10 @@
 package com.thenewmotion.akka
 
 import com.rabbitmq.client.ConnectionFactory
+import akka.actor.{Props, ActorRef}
+import rabbitmq.ConnectionActor.{Created, Create}
+import akka.util.Timeout
+import akka.util.duration._
 
 /**
  * @author Yaroslav Klymko
@@ -8,5 +12,15 @@ import com.rabbitmq.client.ConnectionFactory
 package object rabbitmq {
   implicit def reachConnectionFactory(x: ConnectionFactory) = new {
     def uri: String = "amqp://%s@%s:%d/%s".format(x.getUsername, x.getHost, x.getPort, x.getVirtualHost)
+  }
+
+  implicit def reachConnectionActor(connection: ActorRef) = new {
+    def createChannel(props: Props, name: Option[String] = None)
+                     (implicit timeout: Timeout = Timeout(2 seconds)): ActorRef = {
+      import akka.dispatch.Await
+      import akka.pattern.ask
+      val future = connection ? Create(props, name)
+      Await.result(future, timeout.duration).asInstanceOf[Created].channel
+    }
   }
 }
